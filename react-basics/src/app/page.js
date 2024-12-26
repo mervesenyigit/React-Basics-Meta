70e985a7013d697fea799d27ae8f46ba44261018
mervesnygt <merve.senyigit@outlook.com>
2024-12-26 11:53:51 +0300

    Add props to pass theme state to Nav component

--- react-basics/src/app/page.js
+++ react-basics/src/app/page.js
@@ -1,30 +1,39 @@
+'use client';
+
 import Image from "next/image";
+import Nav from './../components/Nav';
+import Add from './../components/Add';
 
-function Add()
-{
-  var a=10;
-  var b=15;
-
-  var result=a+b;
-  return(
-  <h2>{result}</h2>
+import { useState } from 'react'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-  )
-}
 
 
 
 export default function Home() {
   let title = "Hi Merveeee!";
+  const [theme, setTheme] = useState("light"); // useState ile tema durumunu yönetiyoruz
+
+  // Tema değiştirme fonksiyonu
+  const toggleTheme = () => {
+    setTheme(prevTheme => prevTheme === "light" ? "dark" : "light");
+  };
+
+
   return (
-    <div>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className={theme}> 
+
+      <Nav theme={theme} darkClick={toggleTheme}/>
+
+ 
+
+      {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <h1>{title}</h1>
       </div>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <Add/>
-      </div>
+      </div> */}
     </div>
   );
 }
--- react-basics/src/components/Nav.js
+++ react-basics/src/components/Nav.js
@@ -1,14 +1,27 @@
-function Nav() {
-    return (
-        <nav className="main-nav">
-            <ul>
-                <li>Home</li>
-                <li>Articles</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    );
-};
+'use client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Nav({ theme, darkClick  }) {
+  return (
+    <nav className={`main-nav ${theme === "dark" ? "dark" : "light"}`}>
+      <div className="d-block gap-2">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={darkClick } 
+        >
+          Toggle Dark Mode
+        </button>
+      </div>
+      
+      <ul>
+        <li>Home</li>
+        <li>Articles</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
 
 export default Nav;
