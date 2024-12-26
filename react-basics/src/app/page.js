ee337172175d9102a9abe6c98af84d58173d8abb
mervesnygt <merve.senyigit@outlook.com>
2024-12-26 11:57:42 +0300

    Implemented props to pass calculation result to Add component

--- react-basics/src/app/page.js
+++ react-basics/src/app/page.js
@@ -19,14 +19,16 @@ export default function Home() {
   const toggleTheme = () => {
     setTheme(prevTheme => prevTheme === "light" ? "dark" : "light");
   };
-
+  const a = 10;
+  const b = 15;
+  const result = a + b;
 
   return (
     <div className={theme}> 
 
       <Nav theme={theme} darkClick={toggleTheme}/>
 
- 
+      <Add sonuc={result} />
 
       {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <h1>{title}</h1>
---
+++ react-basics/src/components/Add.js
@@ -0,0 +1,11 @@
+function Add(props)
+{
+    return[
+        <div>
+            <h2>
+           {props.sonuc} 
+            </h2>
+        </div>
+    ]
+}
+export default Add
